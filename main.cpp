7f4e24d8e47d617fcdcb7161e872fd016e55d22a
HamimMahdiLeo <23-52828-2@student.aiub.edu>
2026-07-18 02:00:57 +0600

    Update Student Information

--- main.cpp
+++ main.cpp
@@ -9,9 +9,9 @@ int main() {
     // Student Information
     // ==========================
 
-    string name = "Your Name";
-    string id = "24-XXXXX-X";
-    string section = "A";
+    string name = "Hamim Mehdi Leo";
+    string id = "23-52828-2";
+    string section = "M";
 
     // ==========================
     // Personal Information
